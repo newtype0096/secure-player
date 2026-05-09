6ad76dc1e78cd8ac4baa0e6d2f0b9e513bf06454
newtype0096 <newtype0096@naver.com>
2026-05-09 14:12:04 +0900

    feat: add packaged player export tool
    - Add a console project for exporting packaged player executables
    - Append video bytes to the player executable output
    - Write package footer metadata with video offset and length
    - Include the export project in the solution

---
+++ secure-player-export/Program.cs
@@ -0,0 +1,27 @@
+﻿using System.Buffers.Binary;
+using System.Text;
+
+string playerExePath = args[0];
+string videoPath = args[1];
+string outputExePath = args[2];
+
+byte[] playerBytes = File.ReadAllBytes(playerExePath);
+byte[] videoBytes = File.ReadAllBytes(videoPath);
+
+long videoOffset = playerBytes.Length;
+long videoLength = videoBytes.Length;
+
+using FileStream output = File.Create(outputExePath);
+
+output.Write(playerBytes);
+output.Write(videoBytes);
+
+Span<byte> footer = stackalloc byte[32];
+
+Encoding.ASCII.GetBytes("SPKGFOOT").CopyTo(footer);
+BinaryPrimitives.WriteInt32LittleEndian(footer.Slice(8, 4), 1);
+BinaryPrimitives.WriteInt32LittleEndian(footer.Slice(12, 4), 0);
+BinaryPrimitives.WriteInt64LittleEndian(footer.Slice(16, 8), videoOffset);
+BinaryPrimitives.WriteInt64LittleEndian(footer.Slice(24, 8), videoLength);
+
+output.Write(footer);
